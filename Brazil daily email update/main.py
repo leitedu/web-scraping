6cf929e0cc65e8a77abc6b16c1d1c6fb07e1f857
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-21 17:50:48 -0300

    Define orchestrator

---
+++ Brazil daily email update/main.py
@@ -0,0 +1,22 @@
+from functions import loop_try, send_email
+from dotenv import load_dotenv
+import os
+
+#Load environment variables from .env file
+load_dotenv()
+login = os.getenv('LOGIN')
+senha = os.getenv('PASSWORD')
+pasta = os.getenv('FOLDER')
+to = os.getenv('TO_EMAIL', '')
+
+#Sets day and email's subject
+dia = date.today() + timedelta(-1)
+subject = f'Resumo diário {dia.strftime("%d/%m/%Y")}'
+
+if __name__ == "__main__":
+
+  #Scrapes information from ONS site and returns file path and email's body 
+  attachments, body = loop_try(dia, login, senha, pasta)
+
+  #Writes and send email 
+  send_email(subject, to, body, attachments)

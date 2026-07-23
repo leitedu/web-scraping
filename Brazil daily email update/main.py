686f9ed7b5b977e222b67713eb8ebaa660cae6d2
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-23 19:14:20 -0300

    Refactor variable names for clarity in main.py

--- Brazil daily email update/main.py
+++ Brazil daily email update/main.py
@@ -5,18 +5,18 @@ import os
 #Load environment variables from .env file
 load_dotenv()
 login = os.getenv('LOGIN')
-senha = os.getenv('PASSWORD')
-pasta = os.getenv('FOLDER')
+password = os.getenv('PASSWORD')
+folder = os.getenv('FOLDER')
 to = os.getenv('TO_EMAIL', '')
 
 #Sets day and email's subject
-dia = date.today() + timedelta(-1)
-subject = f'Resumo diário {dia.strftime("%d/%m/%Y")}'
+day = date.today() + timedelta(-1)
+subject = f'Daily Report {day.strftime("%d/%m/%Y")}'
 
 if __name__ == "__main__":
 
   #Scrapes information from ONS site and returns file path and email's body 
-  attachments, body = loop_try(dia, login, senha, pasta)
+  attachments, body = loop_try(day, login, password, folder)
 
   #Writes and send email 
   send_email(subject, to, body, attachments)

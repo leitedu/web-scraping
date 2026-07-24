3eb65657498c8bd6d0b39bac82bcd1557ea3d026
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-24 10:02:42 -0300

    Enhance main.py with browser functions and data scraping
    Refactor email update script to include new functions for browser handling and data scraping.

--- Brazil daily email update/main.py
+++ Brazil daily email update/main.py
@@ -1,22 +1,36 @@
-from functions import loop_try, send_email
+from functions import get_browser, close_browser, repdoe, reservoirs, send_email
 from dotenv import load_dotenv
+from datetime import date, timedelta
 import os
 
 #Load environment variables from .env file
 load_dotenv()
+
+#ONS credentials
 login = os.getenv('LOGIN')
 password = os.getenv('PASSWORD')
-folder = os.getenv('FOLDER')
-to = os.getenv('TO_EMAIL', '')
+folder = os.getenv('FOLDER') #Folder where REPDOE report will be saved - must be informed
+to = os.getenv('TO_EMAIL') #Recipient email address - must be informed
 
 #Sets day and email's subject
 day = date.today() + timedelta(-1)
 subject = f'Daily Report {day.strftime("%d/%m/%Y")}'
 
 if __name__ == "__main__":
+    #Opens browser
+    p, browser, context, page = get_browser()
 
-  #Scrapes information from ONS site and returns file path and email's body 
-  attachments, body = loop_try(day, login, password, folder)
+    #Scrapes information from ONS to download REPDOE if credentials are provided
+    if login and password:
+        attachments = repdoe(page, day, login, password, folder)
+    else:
+        attachments = []
 
-  #Writes and send email 
-  send_email(subject, to, body, attachments)
+    #Writes email's body with reservoir data information
+    body = reservoirs(page, day)
+    
+    #Closes browser
+    close_browser(p, browser)
+
+    #Writes and send email 
+    send_email(subject, to, body, attachments)

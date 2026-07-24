2e473d6446549774b71cdb48d854c70ccb43dafb
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-24 18:11:10 -0300

    Make ID range and parameters configurable
    Updated ID_MIN and ID_MAX to None and made TARGET_DIR and ATTEMPTS configurable.

--- Argentina gas exports/main.py
+++ Argentina gas exports/main.py
@@ -1,12 +1,12 @@
 from scraper import gas_licenses_database
 from processor import scrape_pdf_content
 
-ID_MIN = 590
-ID_MAX = 604
+ID_MIN = None
+ID_MAX = None
 DOWNLOAD = True
-COUNTRY = '' #Name in Spanish
-TARGET_DIR = "./dados_gas"
-ATTEMPTS = 3
+COUNTRY = '' #Name in Spanish, empty for download all countries data
+TARGET_DIR = None
+ATTEMPTS = None
 
 if __name__ == "__main__":
 
